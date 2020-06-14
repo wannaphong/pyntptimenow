367a3200a8fe57971aaf96f67437d4778e1dfe1c
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2020-06-14 12:50:45 +0700

    ntptimenow v0.2

--- setup.py
+++ setup.py
@@ -4,13 +4,18 @@ from setuptools import setup
 with open('README.md','r',encoding='utf-8-sig') as f:
     long_description = f.read()
 
+requirements = [
+    'ntplib'
+]
+
 setup(name='ntptimenow',
-      version='0.1',
+      version='0.2',
       description='Python NTP Time Now',
       author='Wannaphong Phatthiyaphaibun',
       author_email='wannaphong@kkumail.com',
       url='https://github.com/wannaphong/pyntptimenow',
       py_modules=['ntptimenow'],
+      install_requires = requirements,
       license='MIT',
       classifiers=[
           'License :: OSI Approved :: MIT License',
